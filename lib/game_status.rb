65bb8e1ebbeb8dfaae01dbb9ffa4f98ab02ddefb
Natalia Galan <n8galan@gmail.com>
2020-06-14 05:42:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,3 +45,13 @@ WIN_COMBINATIONS.each do |win_combination|
   def full?(board)
      board.all? {|i| i == "X" || i == "O"}
   end
+
+  def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !won?(board) && !full?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
